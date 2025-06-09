9e053100dbda527ddc58c9f2d8a853800ad17da4
Sergio Barreras <sergiobg.isc@gmail.com>
2025-06-08 20:43:46 -0700

    feat: Mostrar y remover favoritos

--- src/components/pokemons/FavoritePokemons.tsx
+++ src/components/pokemons/FavoritePokemons.tsx
@@ -1,5 +1,6 @@
 import type { FavoritePokemon } from "@interfaces/favorite-pokemon";
 import { createSignal, For } from "solid-js";
+import { FavoritePokemonCard } from "./FavoritePokemonCard";
 
 const getLocalStoragePokemons = (): FavoritePokemon[] => {
   const favoritePokemons: FavoritePokemon[] = JSON.parse(
@@ -14,7 +15,9 @@ export const FavoritePokemons = () => {
 
   return (
     <div class="grid grid-cols-2 sm:grid-cols-4">
-      <For each={pokemons()}>{(pokemon) => <span>{pokemon.name}</span>}</For>
+      <For each={pokemons()}>
+        {(pokemon) => <FavoritePokemonCard pokemon={pokemon} />}
+      </For>
     </div>
   );
 };
